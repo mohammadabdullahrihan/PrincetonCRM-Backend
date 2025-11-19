8ec3904b87effb13d02bc7c27cd807a63decf8d9
Your Name <your.email@example.com>
2025-11-19 12:15:42 +0600

    fix big 6

--- src/controllers/pdfController/index.js
+++ src/controllers/pdfController/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const moment = require('moment');
 let pdf = require('html-pdf');
 const { listAllSettings, loadSettings } = require('../../middlewares/settings');
-const { getData } = require('../middlewares/serverData');
+const { getData } = require('../../middlewares/serverData');
 const useLanguage = require('../locale/useLanguage');
 const { useMoney, useDate } = require('../settings');
 
